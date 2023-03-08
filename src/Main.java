21e6596dd8ee5bafb766f6be1a11396e1d9418c8
DenisArger <den.arger@gmail.com>
2023-03-08 11:39:27 +0300

    finnaly

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Model.Application;
+
+// для корректной работы потребуется библиотека json-simple 1.1
+public class Main {
+	public static void main(String[] args) {
+		Application application = new Application();
+		application.run_(); 
+	}
+}
